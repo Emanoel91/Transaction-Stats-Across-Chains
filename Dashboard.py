55b255680c1d8d5a41730967c19f5d8f3a65c146
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-09-19 13:38:17 +0330

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -12,55 +12,9 @@ st.set_page_config(
     page_title="Transaction Stats Across Chains",
     layout="wide"
 )
-
-# --- Sidebar Footer Slightly Left-Aligned ---
-st.sidebar.markdown(
-    """
-    <style>
-    .sidebar-footer {
-        position: fixed;
-        bottom: 20px;
-        width: 250px;
-        font-size: 13px;
-        color: gray;
-        margin-left: 5px; # -- MOVE LEFT
-        text-align: left;  
-    }
-    .sidebar-footer img {
-        width: 16px;
-        height: 16px;
-        vertical-align: middle;
-        border-radius: 50%;
-        margin-right: 5px;
-    }
-    .sidebar-footer a {
-        color: gray;
-        text-decoration: none;
-    }
-    </style>
-
-    <div class="sidebar-footer">
-        <div>
-            <a href="https://x.com/axelar" target="_blank">
-                <img src="https://img.cryptorank.io/coins/axelar1663924228506.png" alt="Axelar Logo">
-                Powered by Axelar
-            </a>
-        </div>
-        <div style="margin-top: 5px;">
-            <a href="https://x.com/0xeman_raz" target="_blank">
-                <img src="https://pbs.twimg.com/profile_images/1841479747332608000/bindDGZQ_400x400.jpg" alt="Eman Raz">
-                Built by Eman Raz
-            </a>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
 # --- Title & Info Messages ---------------------------------------------------------------------------------------------
-st.title("📖Interoperability Overview")
+st.title("⛓Transaction Stats Across Chains")
 
-st.info("📊 Charts initially display data for a default time range. Select a custom range to view results for your desired period.")
 st.info("⏳ On-chain data retrieval may take a few moments. Please wait while the results load.")
 
 # --- Snowflake Connection ----------------------------------------------------------------------------------------
